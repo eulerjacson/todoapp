a456adef671c04b5cbf489c2500ac0f8e42c5597
eulerjacson <euler.empari@gmail.com>
2021-05-28 11:14:39 -0300

    tv changes

--- app/src/main/java/com/ejbtech/todoapp/MainActivity.kt
+++ app/src/main/java/com/ejbtech/todoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ejbtech.todoapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvMain.text = "Este é meu app To Do"
     }
 }
